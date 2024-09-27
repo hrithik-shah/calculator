33d28b0d63d07a04fd76f481d7621358a2c7dbdb
Hrithik Shah <hrithik21.shah@gmail.com>
2024-09-27 17:24:58 -0400

    Add GUI

---
+++ script.js
@@ -0,0 +1,13 @@
+function operate(op, num1, num2) {
+    switch (op) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+    }
+}
+
